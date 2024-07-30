d1d3419db470db451cec9626823321c212b92f58
Marcin Lesiak <marcinui07@gmail.com>
2024-07-30 18:42:35 +0200

    added chats

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -9,43 +9,54 @@ import { Principal } from '@dfinity/principal';
 export default {
   data() {
     return {
-      newNote: "",
-      notes: [] as string[][],
+      newChat: "",
+      chats: [] as string[][],
       identity: undefined as undefined | Identity,
-      principalText: ""
+      principalText: "",
+      targetPrincipal: "",
     }
   },
   methods: {
-    async dodajNotatke() {
+    async dodajChatMSG() {
       if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
         throw new Error("User is not logged in")
       }
+      const targetPrincipal = Principal.fromText(this.targetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()) {
+        throw new Error("Wrong target")
+      }
+
       const backend = createActor(canisterId, {
         agentOptions: {
           identity: this.identity
         }
       });
-      await backend.add_note(this.newNote);
-      await this.pobierzNotatki();
+      await backend.add_chat_msg(this.newChat, targetPrincipal)
+      await this.pobierzChaty()
     },
-    async pobierzNotatki() {
+    async pobierzChaty() {
       if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
         throw new Error("User is not logged in")
       }
-      this.notes = await bootcamp_chat_backend.get_notes(this.identity.getPrincipal())
+      const targetPrincipal = Principal.fromText(this.targetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()) {
+        throw new Error("Wrong target")
+      }
+      this.chats = await bootcamp_chat_backend.get_chat(this.identity.getPrincipal(), targetPrincipal)
     },
     async login() {
       const authClient = await AuthClient.create();
       await authClient.login({
-        identityProvider: "http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943/"
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
       })
+
       const identity = authClient.getIdentity();
-      this.principalText = identity.getPrincipal().toText();
+      this.principalText = identity.getPrincipal().toText()
       console.log("Zalogowano", this.principalText)
       this.identity = identity;
-      await this.pobierzNotatki();
+      await this.pobierzChaty()
     }
-  }
+  },
 }
 </script>
 
@@ -54,12 +65,17 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    {{ principalText }}<button @click="login">Log in</button>
-    <div v-for="note in notes[0]">
-      {{ note }}
+    {{ principalText }} <button @click="login">login</button>
+    <div>
+      <input v-model="targetPrincipal" />
     </div>
     <div>
-      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
+      <div v-for="chat in chats[0]">
+        {{ chat }}
+      </div>
+    </div>
+    <div>
+      <textarea v-model="newChat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
     </div>
   </main>
 </template>
